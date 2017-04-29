a3e82a3a4faac3d1239f0e8eda1587ce3feeffff
Matt Hampel <matth@localdata.com>
2017-04-29 11:28:11 -0400

    Start on matching

---
+++ test/match.js
@@ -0,0 +1,10 @@
+const csv = require('../lib/csv')
+
+const sample = require('./sample')
+
+describe('Match', () => {
+  it('grantees to ledger IDs', function(done) {
+    assert(false)
+    done()
+  })
+})
---
+++ test/sample.js
@@ -0,0 +1,33 @@
+const moment = require('moment')
+module.exports = [
+  {
+    granter_id: 306,
+    title: 'Shannon Cason’s Homemade Stories Live',
+    description: 'To spotlight the best national and local storytellers through a monthly event, Homemade Stories Live.',
+    start: moment('2016-11-05T00:00:00.000'),
+    end: moment('2019-12-01T00:00:00.000'),
+    grantee: 'Shannon Cason',
+    amount: 65000,
+    url: 'https://knightfoundation.org/grants/7795',
+  },
+  {
+    granter_id: 306,
+    title: 'The Enemy of My Enemy',
+    description: 'To explore external narratives and perceptions of the United States by collaborating with artists from China, Russia and Iran to write new works about Americans and perform them in Detroit and online.',
+    start: moment('2016-11-05T00:00:00.000'),
+    end: moment('2019-12-01T00:00:00.000'),
+    grantee: 'The Hinterlands',
+    amount: 70000,
+    url: 'https://knightfoundation.org/grants/7797',
+  },
+  {
+    granter_id: 306,
+    title: 'Wire-Car Auto Workers Association of Detroit (WAWAD)',
+    description: 'To promote wire-car culture through an interactive website that serves as a resource for wire-car makers and enthusiasts, and to create a mobile parking structure to showcase models by association members.',
+    start: moment('2016-11-05T00:00:00.000'),
+    end: moment('2018-12-01T00:00:00.000'),
+    grantee: 'Wire-Car Auto Workers Association of Detroit (WAWAD)',
+    amount: 7300,
+    url: 'https://knightfoundation.org/grants/7814',
+  },
+]
