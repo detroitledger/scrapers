22e11c07c39c32db25e3857356d06472347580b9
Matt Hampel <matth@localdata.com>
2017-05-10 23:15:40 -0400

    Wait this is not testable right now

--- test/match.js
+++ test/match.js
@@ -1,23 +1,13 @@
-const match = require('../lib/match')
+// const match = require('../lib/match')
 
 const sample = require('./sample')
 
-const tests = [
-  {
-    query: 'focus: hope',
-    name: 'Focus: HOPE',
-    id: 223,
-  }, {
-    query:
-  }
-]
-
 describe('Match', () => {
-  it('matches some grantees', function(done) {
-    match.org('focus: hope').then(org => {
-      org.name.should.equal('Focus: HOPE')
-      org.id.should.equal(223)
-      done()
-    })
+  xit('matches some grantees', function(done) {
+    // match.org('focus: hope').then(org => {
+    //   org.name.should.equal('Focus: HOPE')
+    //   org.id.should.equal(223)
+    //   done()
+    // })
   })
 })
